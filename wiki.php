e09b5a96a7a29e8ee44a5c83cd323198084ada77
ManonRenoult <manon.renoult03@gmail.com>
2021-06-27 16:00:43 +0200

    Add files via upload

---
+++ wiki.php
@@ -0,0 +1,28 @@
+<?php include 'menu.php';?>
+    <div class="container containerB">
+        <div class="row maxHeight">
+            <div class="col-lg-5 col-md-12 col-sm-12 boxCard">
+                <div class="cardAll">
+                    <a class="cardLink" href="#"  onclick="document.location.href='./rejoindre.php';">
+                        <div class="btn cardAllJoin">
+                            <div class="cardTitle">
+                                <div class="tucasse">Nous rejoindre</div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="offset-lg-2 col-lg-5 col-md-12 col-sm-12 boxCard mediaQueryCard">
+                <div class="cardAll">
+                    <a class="cardLink" href="#"  onclick="document.location.href='./commandes.php';">
+                        <div class="btn cardCommands">
+                            <div class="cardTitle">
+                                <div class="tucasse">Les commandes</div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include 'footer.php';?>
