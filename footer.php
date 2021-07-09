97f2ecbda6e0cff881aa507a30312b33f53ff32d
RetroManiia <antoine.hu.prof@gmail.com>
2021-07-09 16:44:14 +0200

    modification footer

--- footer.php
+++ footer.php
@@ -3,18 +3,18 @@
         <div class="container containerC">
             <div class="row">
                 <div class="offset-1 col-lg-4 col-md-4 col-sm-12 titelApropos">
-                    <h3>A propos</h3>
+                    <h4>A propos</h4>
                     <p>&nbsp;&nbsp; L-A craft est un serveur semi rp dans lequel vous pouvez construire votre maison, travailler, vous amuser entre amis et bien plus!</p>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-12 titleNousContacter">
-                    <h3>Liens utiles</h3>
+                    <h4>Liens utiles</h4>
                     <li>&nbsp;&nbsp;<a href="#" class="linkWikiFooter" onclick="document.location.href='./wiki.php';">Wiki</a></li><br>
-                    <h3>Nous contacter</h3>
+                    <h4>Nous contacter</h4>
                     &nbsp;&nbsp;<a class="linkWikiFooter" href="mailto:dev@l-a-craft-server.fr">dev@l-a-craft-server.fr </a>
 
                 </div>
                 <div class="col-lg-3 col-md-2 col-sm-12 titleReseauSociaux">
-                    <h3>Réseaux sociaux</h3>
+                    <h4>Réseaux sociaux</h4>
                     <div class="row">
                         <div class="col-3">
                             <a style="text-decoration: none; float: left;" href="#" onclick="window.open('https://discord.gg/9GTTT6y8tH');">&nbsp;&nbsp;<i class="fab fa-discord fa-3x"></i></a>
@@ -24,6 +24,13 @@
                         </div>
                     </div>
 
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-11">
+                    <hr>
+                    <p>Copyright © 2021 L-A.Craft - Tous droits réservés.</p>
                 </div>
             </div>
         </div>
