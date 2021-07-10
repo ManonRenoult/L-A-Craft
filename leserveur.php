2d6972ace0710036016a519c68e1076664bea052
Manon Renoult <manon.renoult03@gmail.com>
2021-07-10 21:47:47 +0200

    ajout des infos de la page le serveur

--- leserveur.php
+++ leserveur.php
@@ -1,30 +1,50 @@
 <?php include 'menu.php';?>
 
     <div class="container containerRejoindre">
+
         <div class="row rejoindre">
             <div class="offset-2 col-8 nousRejoindreexpl">
                 <p>
                     L-A craft est un serveur semi rp dans lequel vous pouvez construire votre maison, travailler, vous amuser entre amis et bien plus!<br>
 
-                    Les batiments principaux <br>
-                    La maison des métiers<br>
-                    Dans la maison des métiers vous pouvez choisir un métier parmi les quatre proposés:<br>
-                <li> Chasseur </li>
-                ○ Gagne de l'argent pour chaque mob tué<br>
-                <li>Mineur</li>
-                ○ Gagne de l'argent pour chaque bloc minerai miné<br>
-                <li>Agriculteur</li>
-                ○ Gagne de l'argent pour chaque graine de récoltée ou plantée<br>
-                <li>Bûcheron</li>
-                ○ Gagne de l'argent pour chaque bloc bois miné<br>
-                La place des téléporteurs<br>
-                Les téléporteurs permettent de se rendre dans les maps pour travailler. L'arène de PvP est aussi accessible à cet endroit.<br>
-                La banque<br>
-                La banque permet de déposer ton argent et de recevoir des intérêts toutes les 25 minutes de jeu.<br>
-                Les magasins<br>
-                Les magasins proposent à la vente toute sorte d'articles (minerais, stuff, nourriture…)<br>
 
                 </p>
+                <button class="accordion" style="font-size: 1.5em">Les métiers</button>
+                <div class="panel">
+                    <p><li style="list-style-type: none;"> Chasseur </li>
+                    &nbsp;&nbsp;&nbsp;Gagne de l'argent pour chaque mob tué<br>
+                    <li style="list-style-type: none;">Mineur</li>
+                    &nbsp;&nbsp;&nbsp;Gagne de l'argent pour chaque bloc minerai miné<br>
+                    <li style="list-style-type: none;">Agriculteur</li>
+                    &nbsp;&nbsp;&nbsp;Gagne de l'argent pour chaque graine de récoltée ou plantée<br>
+                    <li style="list-style-type: none;">Bûcheron</li>
+                    &nbsp;&nbsp;&nbsp;Gagne de l'argent pour chaque bloc bois miné<br></p>
+                </div>
+
+                <button class="accordion" style="font-size: 1.5em">Les lieux</button>
+                <div class="panel">
+
+                    <div style="font-size: 1.25em">La maison des métiers<br></div>
+                    &nbsp;Dans la maison des métiers vous pouvez choisir un métier parmi les quatre proposés<br>
+                    <div style="font-size: 1.25em">La place des téléporteurs<br></div>
+                    &nbsp;Les téléporteurs permettent de se rendre dans les maps pour travailler. L'arène de PvP est aussi accessible à cet endroit.<br>
+                    <div style="font-size: 1.25em">La banque<br></div>
+                    &nbsp;La banque permet de déposer ton argent et de recevoir des intérêts toutes les 25 minutes de jeu.<br>
+                    <div style="font-size: 1.25em">Les magasins<br></div>
+                    &nbsp;Les magasins proposent à la vente toute sorte d'articles (minerais, stuff, nourriture…)<br></p>
+                </div>
+
+                <button class="accordion" style="font-size: 1.5em">Les jeux</button>
+                <div class="panel">
+                    <div style="font-size: 1.25em">Hunger Game<br></div>
+                    &nbsp;Dans ce mode de jeu, tu seras téléporté vers un monde à l'abandon. Tu auras ai début du jeu, 30 secondes pour récupérer le maximum de stuff dans les coffres au centre de la map. Après ce temps, puisse le sort vous être favorable...
+                    <div style="font-size: 1.25em">Bed War<br></div>
+                    Dans Bed Wars, les joueurs apparaissent seuls ou en équipes de quatre joueurs maximum, avec huit ou quatre équipes différentes sur des îles séparées selon les modes. Ces îles ont un lit à l’avant et un générateur de ressources à l’arrière. Ce générateur de ressources fait apparaître du fer et de l’or, ou des émeraudes avec l’amélioration Forge d’émeraude, et ces ressources peuvent être utilisées à la boutique d’objets pour acheter des objets de plusieurs catégories. Les joueurs peuvent se rendre sur d’autres îles de l’équipe et casser leurs lits pour éliminer la méthode de reproduction de l’équipe. Les équipes peuvent défendre le lit en plaçant des blocs par-dessus, avec une sélection de blocs disponibles dans la boutique. Si le lit de l’équipe est cassé par un joueur d’une autre équipe, il ne se reproduira plus et sera éliminé du jeu à sa mort.
+                </div>
+
+
+
+
 
             </div>
         </div>
