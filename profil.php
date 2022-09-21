9fbba27207693937e675817ce47bb5f963abbe8c
RetroManiia <antoine.hu.prof@gmail.com>
2022-09-21 23:51:25 +0200

    profile

--- profil.php
+++ profil.php
@@ -1,121 +1,81 @@
-
 <?php
-include 'menu.php';
+session_start();
 include 'bdd.php';
-
-if (empty ($_SESSION['username'])) {
-    header("Location: ./");
+global $bdh;
+$player = $_GET['player'];
+$rangFinal = '';
+$uuidLuckPerm = '';
+include 'func_getprofile.php';
+$rangColor = '';
+$userNameLower = strtolower($_SESSION['username']);
+$getParams = $bdh->prepare("SELECT * FROM luckperms_players where username = ?");
+$getParams->execute(array($userNameLower));
+$allParamGet = $getParams->fetchAll();
+foreach ($allParamGet as $paramGet) {
+    $getParams3 = $bdh->prepare("SELECT * FROM luckperms_user_permissions where uuid = ?");
+    $getParams3->execute(array($paramGet['uuid']));
+    $allParamGet3 = $getParams3->fetchAll();
+    foreach ($allParamGet3 as $paramGet3) {
+        if(strpos($paramGet3['permission'], 'group.') !== false) {
+            $rang = ucfirst(substr($paramGet3['permission'], 6));
+            if ($rang === 'Vendeur') {
+                $rangColor = '<div class="infoplayer_rank" style="color:yellow !important;"> ' . '[ Vendeur ]' . '</div>';
+            } else if ($rang === 'Moderateur') {
+                $rangColor = '<div class="infoplayer_rank" style="color:#6f42c1 !important;"> ' . '[ Moderateur ]' . '</div>';
+            } else if($rang === 'Admin'){
+                $rangColor = '<div class="infoplayer_rank" style="color:red !important;"> ' . '[ Admin ]' . '</div>';
+            } else if($rang === 'VIP'){
+                $rangColor = '<div class="infoplayer_rank" style="color:red !important;"> ' . '[ VIP ]' . '</div>';
+            } else if($rang === 'Fondateur'){
+                $rangColor = '<div class="infoplayer_rank" style="color:red !important;"> ' . '[ Fondateur ]' . '</div>';
+            } else if($rang === 'Fondateur2'){
+                $rangColor = '<div class="infoplayer_rank" style="color:deeppink !important;"> ' . '[ Fondatrice ]' . '</div>';
+            } else {
+                $rangColor = '<div class="infoplayer_rank" style="color:green !important;"> ' . '[ Membre ]' . '</div>';
+            }
+        }
+    }
 }
-
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="L-A-Craft est un serveur dans lequel vous pourrez avoir votre métier et gagner votre argent pour vous acheter un terrain, des items, services et bien plus !">
+    <meta name="robots" content="index,map,voter,wiki,status">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" type="text/css">
+    <link rel="icon" type="image/png" href="https://l-a-craft.fr/images/Litle-logoLADiscordSF.png">
+    <title>Profile - L-A Craft</title>
+</head>
 
-<div class="container containerProfil zIndex3">
-    <div class="row maxHeight">
-        <div class="col-lg-3 col-md-12 col-sm-12 divPhotoUser">
-            <div class="cardPhotoUser">
-                <div class="photoUser">
-                    <img class="photo" src="images/userPhoto.jpg">
-                </div>
-                <div class="userName">
-                    <?php
-                    if (!empty ($_SESSION['username'])) {
-                        echo $_SESSION['username'];
-                    } else {
-                        echo 'Connecter vous';
-                    }
-                    ?>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-9 col-md-12 col-sm-12 divInfosUser ">
-            <div class="cardInfosUser">
-                <div class="row userInfos">
-                    <div class="col-lg-3 col-md-12 col-sm-12 infosTitle">
-                        Ton rang
-                    </div>
-                    <div class="col-lg-5 col-md-12 col-sm-12 infos">
-                        <?php
-                        if (!empty ($_SESSION['rang'])) {
-                            echo $_SESSION['rang'];
-                        } else {
-                            echo 'Connecter vous';
-                        }
-                        ?>
-                    </div>
-                    <div class="col-lg-4 col-md-12 col-sm-12 fa">
-                        <i class="far fa-flag fa-3x"></i>
-                    </div>
-                </div>
-                <div class="row userInfos">
-                    <div class="col-lg-3 col-md-12 col-sm-12 infosTitle">
-                        Ton métier
-                    </div>
-                    <div class="col-lg-5 col-md-12 col-sm-12 infos">
-                        <?php
-                        if (!empty ($_SESSION['username'])) {
-                            $getParams = $bdh->prepare("SELECT * FROM jobs_users where username = ?");
-                            /*mysqli_real_escape_string($bdh,json_encode($_GET['player']));*/
-                            $getParams->execute(array($_SESSION['username']));
-                            $allParamGet = $getParams->fetchAll();
-                            foreach ($allParamGet as $paramGet) {
-                                $arr = explode(":", $paramGet['quests'], 2);
-                                $jobs = $arr[0];
-                                echo $jobs;
-                            }
-                        } else {
-                            echo 'Connecter vous';
-                        }
-                        ?>
-                    </div>
-                    <div class="col-lg-4 col-md-12 col-sm-12 fa">
-                        <i class="fas fa-briefcase fa-3x"></i>
-                    </div>
-                </div>
-                <div class="row userInfos">
-                    <div class="col-lg-3 col-md-12 col-sm-12 infosTitle">
-                        Ton argent
-                    </div>
-                    <div class="col-lg-5 col-md-12 col-sm-12 infos">
-                        <?php
-                        if (!empty ($_SESSION['moneyEconomy'])) {
-                            echo $_SESSION['moneyEconomy'] . '$';
-                        } else {
-                            echo 'Connecter vous';
-                        }
-                        ?>
-                    </div>
-                    <div class="col-lg-4 col-md-12 col-sm-12 fa">
-                        <i class="fas fa-dollar-sign fa-3x"></i>
-                    </div>
-                </div>
-                <div class="row userInfos">
-                    <div class="col-lg-3 col-md-12 col-sm-12 infosTitle">
-                        Nombre de votes
-                    </div>
-                    <div class="col-lg-5 col-md-12 col-sm-12 infos">
-                        <?php
-                        if (!empty ($_SESSION['username']) && !empty ($_SESSION['mdp'])) {
-                            $getTimeNbVote = $bdh->prepare("SELECT * FROM votes where username = ?");
-                            $getTimeNbVote->execute(array($_SESSION['username']));
-                            $allLastGet = $getTimeNbVote->fetchAll();
-                            foreach ($allLastGet as $passGet) {
-                                $nbVote = (string)$passGet['nbVote'];
-                                #$nbVote = strval($passGet['nbVote']);
-                                echo $nbVote;
-                            }
-                        } else {
-                            echo 'Connecter vous';
-                        }
-                        ?>
+<body>
+<div class="grid">
+    <?php include 'menu.php';?>
+    <div class="body">
+        <div class="body_grid">
+            <div class="container_profil">
+                <div class="profil_image_grid">
+                    <div class="profil_image_index">
 
+                        <?php
+                        echo '<div>' . $_SESSION['username'] . '</div>';
+                        echo $rangColor;
+                        echo '<img draggable="false" src="https://mc-heads.net/body/' .  $_SESSION['username'] . '/30">';
+                        ?>
                     </div>
-                    <div class="col-lg-4 col-md-12 col-sm-12 fa">
-                        <i class="fas fa-thumbs-up fa-3x"></i>
+                    <div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <?php include 'footer.php';?>
 </div>
-
-<?php include 'footer2.php'; ?>
+</body>
+<script src="js/50cab66c4a.js" crossorigin="anonymous" async defer></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/getPlayer.js"></script>
+</html>
